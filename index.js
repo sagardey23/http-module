1fa01af53c9961480bf1304dd36bba9d1975a936
sagardey23 <sagarinthesky@gmail.com>
2022-04-07 18:53:58 +0530

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+// const http = require('http');
+// http.createServer(function(request, response){
+//     // response.write('Hello World');
+//     // response.write("<h1> Hello! </h1>")
+//     let user = { name: 'Sagar', age: 27};
+//     response.write(JSON.stringify(user));
+//     response.end();
+// }).listen(5000)
+
+
+// npm init --y
+
+
